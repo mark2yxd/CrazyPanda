1c0dba83419c24853ec6feb7923df21dcfce2521
xudong.handsome <yinxudong@navercorp.com>
2017-09-28 10:37:47 +0800

    test

---
+++ CrazyPanda/CrazyPanda/Controllers/Main/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CrazyPanda
+//
+//  Created by 尹旭东 on 2017/9/28.
+//  Copyright © 2017年 CP. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
